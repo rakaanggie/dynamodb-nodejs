cc50cefe636b7e23e0cf6f0aca837c6d7e75c17a
rakaanggie <rakaanggie123@gmail.com>
2023-06-28 17:19:26 +0700

    first commit

---
+++ dynamo.js
@@ -0,0 +1,91 @@
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+    region: process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "harrypoter-api";
+
+const getCharacters = async() => {
+    const params = {
+        TableName: TABLE_NAME
+    };
+    const characters = await dynamoClient.scan(params).promise();
+    console.log(characters);
+    return characters;
+};
+
+const getCharacterById = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id,
+        }
+    };
+    return await dynamoClient.get(params).promise();
+};
+
+const addOrUpdateCharacter = async(character) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: character
+    }
+    return await dynamoClient.put(params).promise();
+}
+
+const deleteCharacter = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id,
+        }
+    };
+    return await dynamoClient.delete(params).promise();
+};
+
+module.exports = {
+    dynamoClient,
+    getCharacters,
+    getCharacterById,
+    addOrUpdateCharacter,
+    deleteCharacter
+};
+
+
+//getCharacters();
+//
+//const hp = {
+//    "id": "0",
+//    "name": "Harry Potter",
+//    "alternate_names": [
+//        "The Boy Who Lived",
+//        "The Chosen One"
+//    ],
+//    "species": "human",
+//    "gender": "male",
+//    "house": "Gryffindor",
+//    "dateOfBirth": "31-07-1980",
+//    "yearOfBirth": 1980,
+//    "wizard": true,
+//    "ancestry": "half-blood",
+//    "eyeColour": "green",
+//    "hairColour": "black",
+//    "wand": {
+//        "wood": "holly",
+//        "core": "phoenix feather",
+//        "length": 11
+//    },
+//    "patronus": "stag",
+//    "hogwartsStudent": true,
+//    "hogwartsStaff": false,
+//    "actor": "Daniel Radcliffe",
+//    "alternate_actors": [],
+//    "alive": true,
+//    "image": "https://ik.imagekit.io/hpapi/harry.jpg"
+//};
+//
+//addOrUpdateCharacter(hp)
